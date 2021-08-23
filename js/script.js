e0577dd65b26947910e2a2dc3f99e5aaf9d8c4a2
Jesse Vermeulen <jessevermeulen123@gmail.com>
2021-08-23 21:19:51 +0200

    Avocado home page and winning page

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
 (function () {
   const startButton = document.getElementById("startButton");
   const pauseButton = document.getElementById("pauseButton");
-  const stopButton = document.getElementById("stopButton");
-  const highScoresMenuButton = document.getElementById("highScoresMenuButton");
+  const loseButton = document.getElementById("loseButton");
+  const winButton = document.getElementById("winButton");
+  const winScreenMenuButton = document.getElementById("winScreenMenuButton");
+  const loseScreenMenuButton = document.getElementById("loseScreenMenuButton");
   const startScreen = document.getElementById("startScreen");
   const playScreen = document.getElementById("playScreen");
   const winScreen = document.getElementById("winScreen");
+  const loseScreen = document.getElementById("loseScreen");
   startButton.onclick = (e) => {
     e.preventDefault();
     // We should hide the startScreen, show the playScreen by resetting its CSS class, and reset the board and score
+    loseScreen.style.display = "none";
     startScreen.style.display = "none";
     winScreen.style.display = "none";
     playScreen.style.display = "flex";
@@ -20,9 +24,21 @@
     e.preventDefault();
     // We should pause all the game intervals
   };
-  stopButton.onclick = (e) => {
+  loseButton.onclick = (e) => {
     e.preventDefault();
     // We should hide the playScreen, show the loseScreen by resetting its CSS class, and stop all the game intervals
+    loseScreen.style.display = "flex";
+    startScreen.style.display = "none";
+    playScreen.style.display = "none";
+    winScreen.style.display = "none";
+    // Giving up so lets stop the timers
+    clearInterval(countDownTimerId);
+    clearInterval(timerId);
+  };
+  winButton.onclick = (e) => {
+    e.preventDefault();
+    // We should hide the playScreen, show the loseScreen by resetting its CSS class, and stop all the game intervals
+    loseScreen.style.display = "none";
     startScreen.style.display = "none";
     playScreen.style.display = "none";
     winScreen.style.display = "flex";
@@ -31,9 +47,19 @@
     clearInterval(timerId);
   };
   
-  highScoresMenuButton.onclick = (e) => {
+  winScreenMenuButton.onclick = (e) => {
     e.preventDefault();
     // We should hide the winScreen, show the startScreen by resetting its CSS class
+    loseScreen.style.display = "none";
+    winScreen.style.display = "none";
+    playScreen.style.display = "none";
+    startScreen.style.display = "flex";
+  };
+  
+  loseScreenMenuButton.onclick = (e) => {
+    e.preventDefault();
+    // We should hide the winScreen, show the startScreen by resetting its CSS class
+    loseScreen.style.display = "none";
     winScreen.style.display = "none";
     playScreen.style.display = "none";
     startScreen.style.display = "flex";
